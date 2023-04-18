6cef65f18cc82ed0c95b939cf041d38689581cbf
Marven Cesar <cesarmarven@gmail.com>
2023-04-18 11:39:20 -0400

    Adding Signup/login Functionality

---
+++ public/app.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", event => {
+    firebase.initializeApp({
+        apiKey: "AIzaSyD052QaAfvdf9sdLExCn3d7ijdGNROPUAc",
+        authDomain: "movie-app-full-stack-1.firebaseapp.com",
+        projectId: "movie-app-full-stack-1",
+        storageBucket: "movie-app-full-stack-1.appspot.com",
+        messagingSenderId: "732289375022",
+        appId: "1:732289375022:web:de388fa799e9021c4c38bf",
+        measurementId: "G-GK807470HK"
+    });
+    const app = firebase.app();
+    console.log(app);
+});
+
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+    .then(result => {
+        const user = result.user;
+        document.write(`Hello ${user.displayName}`);
+        console.log(user);
+    })
+    .catch(console.log);
+}
+function emailSignUp() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(result => {
+        const user = result.user;
+        document.write(`Hello ${user.displayName}`);
+        console.log(user);
+    })
+    .catch(console.log);
+}
+function emailLogin() {
+    const email = document.getElementById("login-email").value;
+    const password = document.getElementById("login-password").value;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(result => {
+        const user = result.user;
+        document.write(`Hello ${user.displayName}`);
+        console.log(user);
+    })
+    .catch(console.log);
+}
