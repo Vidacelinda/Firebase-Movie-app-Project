dfa742f2935d4e13b999d1782134ebf0194c6975
bulatovic-anastasija <66039575+bulatovic-anastasija@users.noreply.github.com>
2023-04-30 00:41:39 -0400

    search functionality works with backend now

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ var admin = require("firebase-admin");
 const verifyToken = require('./middlewares/verifyToken');
 
 var cred = require("./credentials.json");
+const request = require('request');
+
 
 admin.initializeApp({
   credential: admin.credential.cert(cred)
@@ -11,7 +13,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const corsOptions = {
-  origin: "http://localhost:5002",
+  origin: "http://localhost:5000",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: [
     "Content-Type",
@@ -26,6 +28,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json()); // To parse JSON request bodies
 
+
 app.get('/protected', verifyToken, (req, res) => {
   res.status(200).json({ message: 'Access granted to protected route', user: req.user });
 });
@@ -33,6 +36,35 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Internal Server Error again');
 });
+
+app.get('/movies', (req, res) => {
+  const apiKey = '2f1775072b783326efa2a8d064dfeb34'; // replace with your actual API key
+  const query = req.query.query;
+
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
+
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const data = JSON.parse(body);
+      if (data.results.length > 0) {
+        const movie = data.results[0];
+        const movieData = {
+          title: movie.title,
+          overview: movie.overview,
+          release_date: movie.release_date,
+          poster_path: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+        };
+        res.send(movieData);
+      } else {
+        res.status(404).send('No movie found.');
+      }
+    } else {
+      res.status(response.statusCode).send(error.message);
+    }
+  });
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
