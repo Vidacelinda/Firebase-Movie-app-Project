5244087344ead5b4506a97a5afb1f0901a96f6f2
bulatovic-anastasija <66039575+bulatovic-anastasija@users.noreply.github.com>
2023-05-03 22:22:36 -0400

    not working

--- ana/server.js
+++ ana/server.js
@@ -1,17 +1,10 @@
-
 var admin = require("firebase-admin");
-const verifyToken = require('./middlewares/verifyToken');
+const verifyToken = require('../backend/middlewares/verifyToken');
 
-const { API_KEY } = require("./config.js");
-// const firebase = require("firebase");
-
-var cred = require("./credentials.json");
+const { API_KEY } = require("../backend/config.js");
+var cred = require("../backend/credentials.json");
 const request = require('request');
-// including fetch for home top 5 movies api request 
 const fetch = require('node-fetch');
-
-// including fetch for home top 5 movies api request 
-
 
 admin.initializeApp({
   credential: admin.credential.cert(cred)
@@ -20,7 +13,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const corsOptions = {
-  origin: "https://movie-app-full-stack-1.web.app",
+  origin: "movie-app-full-stack-1.web.app",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: [
     "Content-Type",
@@ -35,46 +28,16 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json()); // To parse JSON request bodies
 
-
-// Swagger Annotation for the `/protected` endpoint
-/**
-* GET request to a protected endpoint that requires a valid JSON web token 
-* to be passed in the Authorization header.
-* @function GET_Method_VerifyToken
-* @name getProtectedEndpoint
-* @param {object} req - The HTTP request object.
-* @param {object} req.user - The user object extracted from the JSON web 
-* token.
-* @param {object} res - The HTTP response object.
-* @returns {object} - The response object with a status code and JSON data  
-* containing a message and user information.
-* @throws {Error} - If the JSON web token is invalid or not provided, an 
-* error is thrown.
-*/
-
 app.get('/protected', verifyToken, (req, res) => {
   res.status(200).json({ message: 'Access granted to protected route', user: req.user });
 });
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Internal Server Error again');
 });
 
-
-// Swagger Annotation for the `/movies` endpoint
-/**
- * Get movie data for a given search query.
- *
- * @function GET_Method_SearchMovie
- * @param {Object} req - The HTTP request object.
- * @param {Object} res - The HTTP response object.
- * @param {string} req.query.query - The search query string.
- *
- * @throws {Error} - If there's an error while fetching data from the API.
- */
-//search funcitonality 
 app.get('/movies', (req, res) => {
-  
   const query = req.query.query;
 
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`;
@@ -100,16 +63,6 @@ app.get('/movies', (req, res) => {
   });
 });
 
-/**
- * Get the list of top rated movies.
- *
- * @function GET_Method_TopRatedMovies
- * @param {Object} req - The HTTP request object.
- * @param {Object} res - The HTTP response object.
- *
- * @throws {Error} - If there's an error while fetching data from the API.
- */
-//API endpoint that retrieves the top rated movies
 app.get('/movie', (req, res) => {
   const Url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`;
     request(Url, (error, response, body) => {
@@ -122,18 +75,6 @@ app.get('/movie', (req, res) => {
     });
 });
 
-/**
- * Get the list of trending movies and TV shows.
- *
- * @function GET_Method_TrendingMovies
- * @async
- * @param {Object} req - The HTTP request object.
- * @param {Object} res - The HTTP response object.
- * @returns {Promise<void>} - The response is sent back in JSON format.
- *
- * @throws {Error} - If there's an error while fetching data from the API.
- */
-//trending
 app.get('/movietrending', async (req, res) => {
   const url = `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}&language=en-US&page=1`;
   try {
@@ -146,11 +87,5 @@ app.get('/movietrending', async (req, res) => {
   }
 });
 
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-});
-
-
+// Export the Express app as an HTTP function
+exports.app = functions.https.onRequest(app);
